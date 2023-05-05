ecafcf2c836438ef33e34ed746f02ca7608ef069
Kristich Pitrella Mateo Agustin <m.kristich@etec.local>
2023-05-05 09:50:19 -0300

    5-05

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,5 +1,6 @@
 
-let task_container = document.getElementById("task_container")
+let task_container = document.getElementById("task_container");
+let boton_add = document.getElementById("btn_add");
 function create_task(titulo,descripcion ,importancia,fecha) {
     let container = document.createElement("div");
     container.innerHTML = `<div class="card" style="width: 18rem;">
@@ -12,8 +13,26 @@ function create_task(titulo,descripcion ,importancia,fecha) {
       <button type="button" class="btn btn-danger">Eliminar</button>
       </div>
   </div>`
-  task_container.appendChild(container)
+  task_container.appendChild(container);
 }
-for (let i = 0; i < 6; i++) {
-    create_task()
-}
+boton_add.addEventListener("click",create_task);
+
+
+// Ventana modal
+var modal = document.getElementById("ventanaModal");
+
+// Botón que abre el modal
+var boton = document.getElementById("abrirModal");
+
+// Hace referencia al elemento <span> que tiene la X que cierra la ventana
+var span = document.getElementsByClassName("cerrar")[0];
+
+// Cuando el usuario hace click en el botón, se abre la ventana
+boton.addEventListener("click",function() {
+  modal.style.display = "block";
+});
+
+// Si el usuario hace click en la x, la ventana se cierra
+span.addEventListener("click",function() {
+  modal.style.display = "none";
+});
